61bbb4c897a1da21f56027f027a932a2d805a0ba
petryshend <petryshend@gmail.com>
2015-10-31 11:39:42 +0200

    Generate Task entity

---
+++ src/ToBeDoneBundle/Entity/Task.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace ToBeDoneBundle\Entity;
+
+/**
+ * Task
+ */
+class Task
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $body;
+
+    /**
+     * @var \DateTime
+     */
+    private $created;
+
+    /**
+     * @var boolean
+     */
+    private $isFinished;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set body
+     *
+     * @param string $body
+     *
+     * @return Task
+     */
+    public function setBody($body)
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+
+    /**
+     * Get body
+     *
+     * @return string
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    /**
+     * Set created
+     *
+     * @param \DateTime $created
+     *
+     * @return Task
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    /**
+     * Get created
+     *
+     * @return \DateTime
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * Set isFinished
+     *
+     * @param boolean $isFinished
+     *
+     * @return Task
+     */
+    public function setIsFinished($isFinished)
+    {
+        $this->isFinished = $isFinished;
+
+        return $this;
+    }
+
+    /**
+     * Get isFinished
+     *
+     * @return boolean
+     */
+    public function getIsFinished()
+    {
+        return $this->isFinished;
+    }
+}
+
