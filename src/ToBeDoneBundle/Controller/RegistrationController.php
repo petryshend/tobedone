c69f38f164cbe1d6c1a3be1d04fe44fcb34ea969
patrxtar <petryshend@gmail.com>
2015-12-04 18:52:24 +0200

    Unique usernames

--- src/ToBeDoneBundle/Controller/RegistrationController.php
+++ src/ToBeDoneBundle/Controller/RegistrationController.php
@@ -2,14 +2,21 @@
 
 namespace ToBeDoneBundle\Controller;
 
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Form;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 use ToBeDoneBundle\Entity\User;
 use ToBeDoneBundle\Form\Type\UserType;
 
 class RegistrationController extends Controller
 {
+    /**
+     * @param Request $request
+     * @return Response
+     */
     public function registerAction(Request $request)
     {
         $user = new User();
@@ -24,7 +31,13 @@ class RegistrationController extends Controller
 
             $em = $this->getDoctrine()->getManager();
             $em->persist($user);
-            $em->flush();
+
+            try {
+                $em->flush();
+            } catch (UniqueConstraintViolationException $ex) {
+                $this->setErrorFlashMessage($ex);
+                return $this->renderRegistrationPage($form);
+            }
 
             $token = new UsernamePasswordToken($user, null, 'main', $user->getRoles());
             $this->get('security.token_storage')->setToken($token);
@@ -32,9 +45,32 @@ class RegistrationController extends Controller
             return $this->redirectToRoute('to_be_done_homepage');
         }
 
+        return $this->renderRegistrationPage($form);
+    }
+
+    /**
+     * @param Form $form
+     * @return Response
+     */
+    private function renderRegistrationPage(Form $form)
+    {
         return $this->render(
             'ToBeDoneBundle:registration:registration.html.twig',
             ['form' => $form->createView()]
         );
+    }
+
+    /**
+     * @param UniqueConstraintViolationException $ex
+     */
+    private function setErrorFlashMessage(UniqueConstraintViolationException $ex)
+    {
+        switch ($ex->getErrorCode()) {
+            case 7:
+                $this->addFlash('error', 'Username already exists');
+                break;
+            default:
+                $this->addFlash('error', 'Some error occurred during writing to database');
+        }
     }
 }
